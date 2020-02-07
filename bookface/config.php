323b3f8e8d099d9664eed5423b0ed65d8568c109
driftmanager <anderwmo@stud.ntnu.no>
2020-02-07 12:30:39 +0000

    hei

---
+++ bookface/config.php
@@ -0,0 +1,10 @@
+<?php
+$dbhost = "192.168.129.53";
+$dbport = "26257";
+$db = "bf";
+$dbuser = "cisco";
+$dbpassw = '';
+$webhost = '10.212.142.192';
+$weburl = 'http://' . $webhost ;
+$frontpage_limit = 1000;
+?>
